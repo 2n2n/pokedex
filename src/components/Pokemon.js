a3bb742edf5e8f5e1488331978a12dddd0556c5a
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2024-05-12 14:38:55 +0800

    implement pokemon with AppJS

---
+++ src/components/Pokemon.js
@@ -0,0 +1,12 @@
+const Pokemon = ({ data }) => {
+    return (
+        <>
+            <img src={data.image} alt="logo" />
+            <p>
+                {data.name}
+            </p>
+        </>
+    )
+}
+
+export default Pokemon;
