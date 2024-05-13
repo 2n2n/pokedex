60a96976de2424e13f3040362b20c564e93feffe
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2024-05-13 16:32:57 +0800

    force first letter of pokemon to be uppercase

--- src/components/PokemonCardComponent.js
+++ src/components/PokemonCardComponent.js
@@ -1,4 +1,7 @@
+import NameFy from "../helpers/word-helper";
+
 const PokemonCardComponent = ({ pokemon }) => {
+    const pokemonName = NameFy(pokemon.name)
     return (
         <div className="card" style={{ 
             width: '18rem'
@@ -7,8 +10,8 @@ const PokemonCardComponent = ({ pokemon }) => {
                 src={pokemon.sprites.front_default} 
                 className="card-img-top" alt="bulbasaur" />
             <div class="card-body">
-                <h5 className="card-title">{pokemon.name}</h5>
-                <a href="#" class="btn btn-primary">{`View ${pokemon.name}`}</a>
+                <h5 className="card-title">{pokemonName}</h5>
+                <a href="#" class="btn btn-primary">{`View ${pokemonName}`}</a>
             </div>
         </div>
     )
