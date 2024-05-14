c345c39e421fb24466c66920373f8efd2bf494e9
danteprogio <potpotprogio5@gmail.com>
2024-05-14 08:53:09 +0800

    adding navbar

---
+++ src/components/PokemonHeader.js
@@ -0,0 +1,29 @@
+const PokemonHeader= ()=>{
+    return(
+        <div className="container">
+            <center>
+                <img 
+                src='https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg'
+                className="card-img-top" height={150} />
+            </center>
+            <div className="container mt-3">
+                <ul className="nav justify-content-center">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">📱 Pokedex</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">🐉Legendary Pokemon</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">✨Shiny Pokemon</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">💁🏻‍♂️About Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonHeader;
