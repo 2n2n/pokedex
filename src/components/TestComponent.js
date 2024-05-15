bc6fcc4725eca5b652348f6815cdfdb173793e5c
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2024-05-15 11:08:12 +0800

    commit implementation of prettierjs and installed axios
    Co-authored-by: sunghajung0722 <sunghajung0722@users.noreply.github.com>
    Co-authored-by: danteprogio <danteprogio@users.noreply.github.com>

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from 'react';
 
 /**
  *
@@ -15,58 +15,44 @@ const TestComponent = () => {
   };
 
   return (
-<>
-<h1 ref={h1Ref}>Clicks: {counter}</h1>
-<button onClick={onClickHandler}>Click Me</button>
-<hr />
-<button
-type="button"
-class="btn btn-primary"
-data-bs-toggle="modal"
-data-bs-target="#staticBackdrop"
->
-Launch static backdrop modal
-</button>
+    <>
+      <h1 ref={h1Ref}>Clicks: {counter}</h1>
+      <button onClick={onClickHandler}>Click Me</button>
+      <hr />
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Launch static backdrop modal
+      </button>
 
-<div
-class="modal fade"
-id="staticBackdrop"
-data-bs-backdrop="static"
-data-bs-keyboard="false"
-tabindex="-1"
-aria-labelledby="staticBackdropLabel"
-aria-hidden="true"
->
-<div class="modal-dialog">
-<div class="modal-content">
-<div class="modal-header">
-<h5 class="modal-title" id="staticBackdropLabel">
-  Modal title
-</h5>
-<button
-  type="button"
-  class="btn-close"
-  data-bs-dismiss="modal"
-  aria-label="Close"
-></button>
-</div>
-<div class="modal-body">...</div>
-<div class="modal-footer">
-<button
-  type="button"
-  class="btn btn-secondary"
-  data-bs-dismiss="modal"
->
-  Close
-</button>
-<button type="button" class="btn btn-primary">
-  Understood
-</button>
-</div>
-</div>
-</div>
-</div>
-</>
+      <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">
+                Modal title
+              </h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">...</div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Understood
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
