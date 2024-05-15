7b4211195400d3df1be20f3a0d521dbb86cda621
danteprogio <potpotprogio5@gmail.com>
2024-05-15 11:07:37 +0800

    Prettier

--- src/components/PokemonHeader.js
+++ src/components/PokemonHeader.js
@@ -1,43 +1,39 @@
 const PokemonHeader = () => {
-  return (
-    <div className="container ">
-      <center>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
-          className="card-img-top"
-          height={150}
-        />
-      </center>
-      <div className="container mt-3">
-        <ul className="nav justify-content-center">
-          <li className="nav-item">
-            <a
-              className="nav-link active text-dark"
-              aria-current="page"
-              href="/"
-            >
-              📱 Pokedex
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="#">
-              🐉Legendary Pokemon
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="/shinypokemon">
-              ✨Shiny Pokemon
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-dark" href="#">
-              💁🏻‍♂️About Us
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container ">
+			<center>
+				<img
+					src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
+					className="card-img-top"
+					height={150}
+				/>
+			</center>
+			<div className="container mt-3">
+				<ul className="nav justify-content-center">
+					<li className="nav-item">
+						<a className="nav-link active text-dark" aria-current="page" href="/">
+							📱 Pokedex
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-dark" href="#">
+							🐉Legendary Pokemon
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-dark" href="/shinypokemon">
+							✨Shiny Pokemon
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link text-dark" href="#">
+							💁🏻‍♂️About Us
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default PokemonHeader;
