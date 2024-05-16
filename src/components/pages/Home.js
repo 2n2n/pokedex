2a1366785a3ee36f984ed10e368f2bbeb9a9c104
Anthony Yolach Lloveras <innoendo.itsolutions@gmail.com>
2024-05-16 16:06:19 +0800

    api example

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,19 @@
-import { useLocation } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
-import NavigationComponent from "../NavigationComponent";
-import TestComponent from "../TestComponent";
+import NavigationComponent from '../NavigationComponent';
+import TestComponent from '../TestComponent';
+import { useEffect } from 'react';
+import { getPokemonByName, getPokemonList } from '../../requests/pokemon-api';
+
 const Home = () => {
   let { state } = useLocation();
+
+  useEffect(() => {
+    getPokemonList(10).then((data) => {
+      console.log('🚀 ~ .then ~ data:', data);
+    });
+  }, []);
+
   return (
     <>
       <h1>Welcome Home! {state?.some}</h1>
--- src/requests/pokemon-api.js
+++ src/requests/pokemon-api.js
@@ -1,1 +1,15 @@
-export const getPokemonByName = () => {};
+import axios from 'axios';
+
+export const getPokemonByName = async (pokemonName = 'ditto') => {
+  return await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+};
+
+export const getPokemonList = async (limit = 10) => {
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
+  return data;
+};
+
+export const getPokemonMetaByURL = async (url) => {
+  const { data } = await axios.get(url);
+  return data;
+};
